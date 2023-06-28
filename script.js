30e4adba3a0c59adb4be78f75daae0d4090fff2c
DilipGelot <dilipgelot16@gmail.com>
2023-06-28 13:13:53 +0530

    update

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-var temperature = document.getElementById("temp");
-var units = document.getElementById("temp_diff");
-var result = document.getElementById("result");
+function convertTemperature() {
+  var input = document.getElementById("input").value;
+  var unit = document.getElementById("unit").value;
+  var resultElement = document.getElementById("result");
 
-var convert = function() {
-  var value = temperature.value;
-  var unit = units.value;
-
-  if (unit === "cel") {
-    result.textContent = value + "°C";
-  } else if (unit === "fah") {
-    result.textContent = (value * 9 / 5) + 32 + "°F";
-  } else if (unit === "kel") {
-    result.textContent = value + "°K";
+  if (isNaN(input)) {
+    resultElement.innerHTML = "Invalid input";
+    return;
   }
-};
 
-temperature.addEventListener("input", convert);
-units.addEventListener("change", convert);
+  var convertedTemperature;
+  var resultText;
+
+  if (unit === "celsius") {
+    convertedTemperature = (input * 9 / 5) + 32;
+    resultText = input + " °C = " + convertedTemperature + " °F";
+  } else if (unit === "fahrenheit") {
+    convertedTemperature = (input - 32) * 5 / 9;
+    resultText = input + " °F = " + convertedTemperature + " °C";
+  } else if (unit === "kelvin") {
+    convertedTemperature = parseFloat(input) - 273.15;
+    resultText = input + " K = " + convertedTemperature + " °C";
+  }
+
+  resultElement.innerHTML = resultText;
+}
